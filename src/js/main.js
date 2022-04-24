adb9d13633b2e9afa06529954f4d6bd15cd17715
jswebschmiede <joergschoeneburg@freenet.de>
2022-04-24 12:55:31 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+import "../sass/main.scss";
+// import "../img/scott-graham-5fNmWej4tAA-unsplash.jpg";
+// import "../img/house-heart-fill.svg";
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "../**.{html,php}",
+    "../**/**.{html,php}",
+    "./src/js/**.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
